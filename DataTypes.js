7dda85113eff80368d2a1db230cdaebc9112c221
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-07 12:09:24 +0530

    7/9/25

---
+++ DataTypes.js
@@ -0,0 +1,22 @@
+/* DataTypes in Java
+Primitve data types
+
+1)Number
+2)Boolean
+3)String
+4)Undefined
+5)Null
+6)Bigint
+7)Symbol
+
+
+1)Number --
+eg : a=23
+    typeof a 
+        -> gives number coz a=23 is of number datatype
+
+2)String 
+eg: name="Catherine"
+typeof a
+    ->gives string as op as name is of string type
+*/
