580df3cf9aeb87b7c0c19df3e45983bddf0e741a
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-07 11:52:03 +0530

    initial commit

---
+++ Lesson1.js
@@ -0,0 +1,8 @@
+/* 
+ This is an intro lesson to JavaScript
+ In Js we use REPL : Read Evaluate Print Loop 
+ It uses Read Evaluate Print then loops again. 
+ We mainly use CONSOLE by inspecting our page.
+ Extension of Javascript file is: js
+
+*/
