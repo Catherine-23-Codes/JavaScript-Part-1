9d9688574ce11dcceab53c50ec81c777b1e0fe24
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2025-09-07 11:59:59 +0530

    7/9/25

---
+++ Variable.js
@@ -0,0 +1,11 @@
+/*
+Variable = name of storage location
+eg: 
+// Integers - they must be initialised with a variable
+    age=23
+    a=5
+
+// Strings - they must be initialised with a variable and also within the double quotes
+    name="catherine"
+    animal="lion"
+*/
