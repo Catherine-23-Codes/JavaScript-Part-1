a35e0817409873185c5d7bb672659d421f7c4534
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-01-03 13:43:49 +0530

    3/1

---
+++ Basics/DataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS Code as newer version
+
+//alert("hello"); we are using Nodejs, not browser
+
+// https://tc39.es/ecma262/ -->refer for rules like MDN
+
+let name = "Catherine";
+let age = 21;
+let isLoggedIn = true;
+
+/*DataTypes
+PRIMITVE ---
+1) number
+2) bigint
+3) string => " "
+4) boolean => true / false
+5) null -> standalone value
+6) undefined -> eg- let accountName; - where we declare a variable+ variable name 
+                                        but value is not assigned
+7) symbol => uniqueness
+
+NON PRIMITVE
+1) object
+
+*/
+
+console.log(typeof "vinaya");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); // o/p- object
+console.log(typeof undefined); // o/p- undefined
