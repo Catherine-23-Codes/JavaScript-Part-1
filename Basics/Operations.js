cf6ce4420c999e22eacc761c167b8c6648428246
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-01-04 09:42:01 +0530

    4/1

---
+++ Basics/Operations.js
@@ -0,0 +1,38 @@
+let value = 33;
+let negValue = -value;
+console.log(negValue);
+
+// basic arithmetic operations
+/*console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 9);
+console.log(2 % 7);
+console.log(2 ** 4);
+*/
+
+//string concatenation
+let str1 = "Hello";
+let str2 = " Catherine";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 3);
+console.log(1 + "7");
+console.log("2" + "9");
+console.log("1" + 5 + 2);
+console.log(2 + 4 + "7");
+
+console.log((3 * 4) / 5 + (8 % 9));
+
+// bad practices
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 8 + 5;
+console.log(num3);
+
+//refer -
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
